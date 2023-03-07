986a5c306594cc925742eefb0d36db6ba5430b63
Spectred <1505073336@qq.com>
2023-03-07 14:19:57 +0000

    Deploying to gh-pages from @ Spectred/spectred.github.io@fb1575b8048082363d749fce8005a83ae5742e95 🚀

---
+++ assets/mermaid-mindmap.esm.min-0350c07a.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-3d92da55.js";import"./framework-b6120433.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
